da29e0bfb7017a12cf70454b394b7df3e3887266
Wistronits <pa022069@gmail.com>
2023-04-07 16:01:54 +0800

    fix

--- routes/verify.js
+++ routes/verify.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { createConnection } = require("../helpers/mysql");
 const verify = require("../helpers/verify");
 require("dotenv").config();
 
@@ -7,11 +8,21 @@ router.use(verify);
 
 router.route("/").get(async function (req, res) {
   try {
-    res.status(200).json({
-      verify: {
-        username: res.locals.username,
-      },
-    });
+    const connection = createConnection();
+    connection.connect();
+    connection.query(`SELECT username, stagename FROM employeeData WHERE username = ?`, res.locals.username, (err, rows) => {
+      if (err) throw err;
+      const resultData = Object.values(JSON.parse(JSON.stringify(rows)));
+
+      res.status(200).json({
+        verify: {
+          username: res.locals.username,
+          stagename: resultData[0].stagename
+        },
+      });
+      
+      connection.end();
+    })
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "err" });
